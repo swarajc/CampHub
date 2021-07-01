d7e9db13287412b3aa6f007b4d29ef0260005faa
Swaraj <swarajchavan540@gmail.com>
2021-07-01 20:24:21 +0530

    update dependencies, resolved ejs syntax errors

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    flash       = require("connect-flash"),
-    passport    = require("passport"),
-    LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
-    Campground  = require("./models/campground"),
-    User        = require("./models/user"),
-    seedDB      = require("./seeds");
-    
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  Campground = require("./models/campground"),
+  User = require("./models/user"),
+  seedDB = require("./seeds");
+
 //requiring routes
 var campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes      = require("./routes/index");
-    
-mongoose.connect("mongodb://localhost/camp_hub_v13");
-app.use(bodyParser.urlencoded({extended: true}));
+  indexRoutes = require("./routes/index");
+
+mongoose.connect("mongodb://localhost/camp_hub_v13", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
@@ -23,27 +26,29 @@ app.use(flash());
 // seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "Once again Rusty wins cutest dog!",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   res.locals.error = req.flash("error");
-   res.locals.success = req.flash("success");
-   next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
-app.listen(3000, function(){
-   console.log("The CampBook Server Has Started!");
+app.listen(3000, function () {
+  console.log("The CampBook Server Has Started!");
 });
